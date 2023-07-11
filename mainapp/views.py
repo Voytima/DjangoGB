93995456c2abffa9259a0876aca3b6ec6f4a4456
Dmitry Voytik <voytima@gmail.com>
2023-07-11 19:46:43 +0300

    Homework to lesson_3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,6 @@
+from typing import Any, Dict
 from django.views.generic import TemplateView
+from datetime import datetime
 
 
 class MainPageView(TemplateView):
@@ -8,6 +10,16 @@ class MainPageView(TemplateView):
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        context["news_title"] = "First test header"
+        context["news_preview"] = "Preliminary first news description"
+
+        context['range'] = range(5)
+        context['datetime_obj'] = datetime.now()
+
+        return context
 
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
